3118721708e422d7ef8e2f84305cfbd2f52eea47
yttap8 <moc.liamg.patrick@gmail.com>
2021-10-07 11:16:47 -0400

    maincode

---
+++ main.js
@@ -0,0 +1,44 @@
+var price = 10;
+var day = "";
+var age = 0;
+var code = "";
+
+function changePriceDay(){
+  day = document.getElementById("Day").value;
+  if (day == "Saturday"){
+    price = 15;
+  }else if (day == "Sunday"){
+    price = 15;
+  }else if (day == "Monday"){
+    price =5;
+  }
+}
+
+function changePriceAge(){
+  age = document.getElementById("Age").value;
+  if (age<=5){
+    price=5;
+  }else if (age>=65){
+    price=5;
+  }
+}
+
+function couponCode(){
+  code = document.getElementById("Code").value;
+  if (code == "APCSP" && day == "Tuesday"){
+    price =5;}
+  if (code == "APCSP" && day == "Wednesday"){
+    price =5;}
+  if (code == "APCSP" && day == "Thursday"){
+    price =5;}
+  if (code == "APCSP" && day == "Friday"){
+    price =5;}
+}
+
+function displayPrice(){
+  price = 10;
+  changePriceAge();
+  changePriceDay();
+  couponCode();
+  document.getElementById("Price").innerHTML = price;
+}
